069b28b6fdd508b1dd03ac0063081ce146d0644d
BATMAN\nordb <tjonk11@gmail.com>
2025-04-16 18:49:22 +0200

    Finished project

--- main.py
+++ main.py
@@ -25,6 +25,15 @@ send_button = tk.Button(root, text="Send", command=send_message)
 send_button.pack(pady=5)
 
 
+message_label = tk.Label(root, text="Message:", justify="left")
+message_label.pack()
+
+def fetch_messages():
+    messages = messages_col.find().sort("_id")
+    message_label.config(text="Messages:\n " + "\n".join(f"- {m["text"]}" for m in messages))
+    root.after(2000, fetch_messages)
+
+fetch_messages()
 root.mainloop()
 
 
