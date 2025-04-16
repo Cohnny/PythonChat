80f47c41dc1255a2c75438de768624beb1686a9a
BATMAN\nordb <tjonk11@gmail.com>
2025-04-16 18:10:59 +0200

    Added mongodb client and basic setup

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 import tkinter as tk
 import pymongo
+
+#Connect to MongoDB
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["PythonChat"]
+messages_col = db["messages"]
+
+
+# GUI
+root = tk.Tk()
+root.title("Python Chat")
+root.geometry("300x300")
+
+
+entry = tk.Entry(root, width=40)
+entry.pack(pady=5)
+
+
+root.mainloop()
+
+
+
+
+# git status
+# git add .
+# git commit -m"<Kommentar>"
+# git push
